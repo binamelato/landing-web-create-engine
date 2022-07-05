21fd929f129d3ecfae48b4cf39541f7eda86d432
Ilos <shahral@mail.ru>
2022-07-05 21:57:46 +0300

    Update gen0.php

--- func/gen0.php
+++ func/gen0.php
@@ -2,8 +2,8 @@
 $link = mysqli_connect( 
             'localhost',
             'user', 
-            'M2Vh6jn5aGpKW5UA',
-            'ok_ld');
+            '',
+            '');
 mysqli_query($link, "SET NAMES utf8");
 if (!$link) { 
    printf("Connection is lost. Error:: %s\n", mysqli_connect_error()); 
